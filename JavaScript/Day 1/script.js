dfa25fc0e3dcfa80999705b058fec29669c451a3
Vivek Mane <vivekmane172@gmail.com>
2025-06-08 17:48:31 +0530

    JavaScript Basics

---
+++ JavaScript/Day 1/script.js
@@ -0,0 +1,18 @@
+alert("Hello world");
+console.log("Code is running");
+console.log("Code is also running");
+console.log("Code is and also running");
+
+var a = prompt("Enter your number");
+
+var isTrue = confirm("Are you sure");
+
+if (isTrue) {
+  console.log("By By");
+} else {
+  console.log("Welcome");
+}
+console.log("Number is " + a);
+document.title = "Hey i am good"
+
+document.body.style.backgroundColor = "red"
---
+++ JavaScript/Day 2/script.js
@@ -0,0 +1,30 @@
+console.log("Hello world!");
+var a = 5;
+var b = 6;
+var c = "vivek";
+console.log(a + b + 8);
+console.log(typeof(a),typeof(b),typeof(c))
+
+
+let x = "hello there"
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x,y,z,p,q,r)
+console.log(typeof(x),typeof(y),typeof(z),typeof(p),typeof(q),typeof(r))
+
+
+let o = {
+    "name": "danny",
+    "job code": 5600,
+    "is handsome":true
+}
+
+console.log(o)
+o.salary = "100cr"
+console.log(o)
+o.salary = "500cr"
+console.log(o)
