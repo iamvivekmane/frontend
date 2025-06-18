76aabc6767bcb2df0c3135c120eb48c9018d2725
Vivek Mane <vivekmane172@gmail.com>
2025-06-18 18:58:39 +0530

    Events in JavaScript

---
+++ JavaScript/Day 12/script.js
@@ -0,0 +1,22 @@
+let button = document.getElementById("btn");
+
+// //For single click
+// button.addEventListener("click", () => {
+//   alert("I was clicked!!!");
+// });
+
+// //For double click
+// button.addEventListener("dblclick", () => {
+//   document.querySelector(".box").innerHTML = "<b>I am clicked twice</b>";
+// });
+
+// //For double click
+// button.addEventListener("contextmenu", () => {
+//   document.querySelector(".box").innerHTML = "<b>You just right clicked</b>";
+// });
+
+// //For pressing any key on keyboard
+// document.addEventListener("keydown", (e) => {
+//   console.log(e.key);
+// });
+
