db2c7f953683dafbc30758c2477f0b4919e228a1
Vivek Mane <vivekmane172@gmail.com>
2025-07-07 17:34:38 +0530

    JavaScript Questions

--- JavaScript/Day 19/Q2.js
+++ JavaScript/Day 19/Q2.js
@@ -1,16 +1,15 @@
-let arr =[ 10,10,30,30,50];
+let arr =[ 10,10,20,30,40];
 
 function doubleTrouble(arr){
     for(let i = 0;i<arr.length;i++){
-        // if(arr[i]==arr[i+1]){
-        //     console.log(arr[i]);
-        //     console.log(arr[i+1]);
-        //     arr[i+1] = arr[i+1]*2;
-        //     continue;
-        // }
-        // else{
-        // }
-        arr[i]= arr[i] * 2;
+        if(arr[i]==arr[i+1])
+        {
+            // console.log("skipping ",i);
+            continue;
+        }
+        else{
+            arr[i] =arr[i]*2;
+        }
     }
     return arr;
 }
