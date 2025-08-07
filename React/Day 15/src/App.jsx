b76a4bacda95651f319f79a1b3c8f9af2b0e16d0
Vivek Mane <vivekmane172@gmail.com>
2025-08-07 21:11:17 +0530

    Connect React to Express

--- React/Day 15/src/App.jsx
+++ React/Day 15/src/App.jsx
@@ -1,33 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useForm } from "react-hook-form"
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    register,
+    handleSubmit,
+    setError,    
+    formState: { errors,isSubmitting },
+  } = useForm();
+
+  const delay =  (d)=>{
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        resolve()
+      },d*1000)
+    })
+  }
+   const onSubmit = async(data) =>{
+    // await delay(2) // It is to simulate the real world network delay
+
+    let r = await fetch("http://localhost:3000/", {method:"POST",headers: {
+      "Content-Type": "application/json",},body: JSON.stringify(data)})
+    let res= await r.text()
+
+     console.log(data,res)
+    //  if(data.username !== "shubham")
+    //  {
+    //    setError("myform", {message:"you are not shubham"})
+    //  }
+    //  if(data.username === "rohan")
+    //  {
+    //   setError("blocked", {message:"you are blocked"})
+    //  }
+    }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    {isSubmitting && <div>Loading...</div>}
+      <div className="container">
+        <form action="" onSubmit={handleSubmit(onSubmit)}>
+          <input placeholder='username'  {...register("username", {required: {value: true, message: "This is a required field"}, minLength : {value : 3, message: "Min length is 3"} , maxLength :{value : 8, message: "Max length is 8"}})} type="text" />
+          {errors.username && <div> {errors.username.message}</div>}
+          <input placeholder='password'  {...register("password" , {minLength : {value : 7, message: "Min length of password is 7"}})} type="password"/>
+          {errors.password && <div> {errors.password.message}</div>}
+          <input disabled={isSubmitting} type="submit" value="submit" />
+          {errors.myform && <div> {errors.myform.message}</div>}
+          {errors.blocked && <div> {errors.blocked.message}</div>}
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
