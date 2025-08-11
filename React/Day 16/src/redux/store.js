841c9005b52141f4f0e1cb050a1514cd82ba1ef0
Vivek Mane <vivekmane172@gmail.com>
2025-08-11 17:41:11 +0530

    Redux toolkit

--- React/Day 16/src/redux/store.js
+++ React/Day 16/src/redux/store.js
@@ -3,8 +3,6 @@ import counterReducer from "./counter/counterSlice"
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer,
+        counter : counterReducer,
     },
 })
-
-// https://stackoverflow.com/questions/54385323/what-is-a-difference-between-action-reducer-and-store-in-redux
